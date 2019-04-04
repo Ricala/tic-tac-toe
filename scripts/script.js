54119d14768aaa9381f7bb856ce3997fa9f1048c
Eric <ericszywala@gmail.com>
2019-04-04 00:36:25 -0400

    Add player choice logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,103 @@
 const boardArea = document.querySelector('.board');
-
+let playerChoice = "";
 
 const gameBoard = (() => {
-  const playerChoice = "X";
-  const gameBoard = [0,1,2,"X",4,5,6,"X","O"];
+  const gameBoard = [0,1,2,3,4,5,6,7,8];
   
   function renderBoard(){
     for(let i = 0; i < 9; i++) {
       if(gameBoard[i] == "X")
       {
+        //Check if spot already has a child
         let xSpot = document.querySelector(`.spot${i}`);
-        let xText = document.createElement('h1');
-        xText.appendChild(document.createTextNode("X"));
-        xSpot.appendChild(xText);
-
-      } else if (gameBoard[i] == "O")
-      {
-        let oSpot = document.querySelector(`.spot${i}`);
-        let oText = document.createElement('h1');
-        oText.appendChild(document.createTextNode("O"));
-        oSpot.appendChild(oText);
+        if(!xSpot.hasChildNodes()) {
+          let xText = document.createElement('h1');
+          xText.appendChild(document.createTextNode("X"));
+          xSpot.appendChild(xText);
+        }
+      } else if (gameBoard[i] == "O") {
+          //check if spot already has a child
+          console.log("Should be O");
+          console.log(playerChoice);
+          let oSpot = document.querySelector(`.spot${i}`);
+          if(!oSpot.hasChildNodes()) {
+            let oText = document.createElement('h1');
+            oText.appendChild(document.createTextNode("O"));
+            oSpot.appendChild(oText);
+          }
       }
-      
-
-      // eachSpace.addEventListener("click", function() {
-      //   checkArea(gameBoard[i], i);
-      //});
-
     }
   };
-
-  renderBoard();
 
 
   const addListeners = (() => {
     for (let i = 0; i < gameBoard.length; i++) {
       let spot = document.querySelector(`.spot${i}`)
       spot.addEventListener("click", function() {
-        console.log("you clicked region number " + i);
+        checkSpot(gameBoard[i], i)
       });
     }
   })();  
 
 
-  function checkArea (area, index) {
-    if (area == ""){
-      setArea(index);
+  function checkSpot (spot, index) {
+    if (spot !== "X" && spot !== "O"){
+      setSpot(index);
     }
   }
   
-  function setArea (index) {
+  function setSpot (index) {
     gameBoard[index] = playerChoice;
-    createBoard();
+    renderBoard();
   };
 
   return {gameBoard};
 })();
 
 console.log(gameBoard);
+
+const player = (() => {
+
+  const playerBtnListener = (() => {
+    let xBtn = document.querySelector('#x-btn');
+    let oBtn = document.querySelector('#o-btn');
+
+    xBtn.addEventListener("click", function() {
+      setPlayer("X");
+      disableBtn();
+    })
+
+    oBtn.addEventListener("click", function() {
+      setPlayer("O");
+      disableBtn();
+    })
+
+    function disableBtn() {
+      xBtn.disabled = true;
+      oBtn.disabled = true;
+    }
+
+    function enableBtn() {
+      xBtn.disabled = false;
+      oBtn.disabled = false;
+    }
+
+    function setPlayer(choice) {
+      playerChoice = choice;
+      console.log("playerchoice set")
+      console.log(playerChoice);
+    }
+
+    function getPlayerChoice(){
+      return playerChoice;
+    }
+
+    return {
+      getPlayerChoice
+    }
+  })();;
+
+  return {
+    playerBtnListener
+  }
+})();
